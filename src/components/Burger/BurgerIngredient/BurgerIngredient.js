f759a98083697b9d6acbeb7e0f1ea5c48282212e
Hudson Pereira <hudson.byte@gmail.com>
2017-12-04 16:41:24 -0200

    🍔 rendering burger

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -31,6 +31,8 @@ class BurgerIngredient extends Component {
       case 'salad':
         ingredient = <div className={classes.Salad}></div>
         break;
+      default:
+        ingredient = null
     }
 
     return ingredient;
