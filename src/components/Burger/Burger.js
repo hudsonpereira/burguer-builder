90b800a20d3517fcce40195ca2f19e51f53c4ffd
Hudson Pereira <hudson.byte@gmail.com>
2017-12-04 17:07:32 -0200

    initial state to BurgerBuilder component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map((key) => {
       return [...Array(props.ingredients[key])].map((_, index) => {
         return (
           <BurgerIngredient key={key + index} type={key} />
         )
       })
-    });
+    }).reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
 
-  // return transformedIngredients;
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = (
+      <p>Please start adding ingredients!</p>
+    );
+  }
 
   return (
     <div className={classes.Burger}>
