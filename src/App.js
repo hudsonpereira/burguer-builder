d1f84d279cff1d1c53f25b17e059a63c1e2d5043
Hudson Pereira <hudson.byte@gmail.com>
2017-12-04 16:12:59 -0200

    root setup, layout setup, hoc aux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div className="">
+        <Layout>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est voluptates quisquam quos cupiditate saepe molestias voluptas ullam obcaecati inventore? Autem amet nam voluptatum iusto perspiciatis enim aliquam magni expedita sapiente, recusandae explicabo quae quaerat quam. Expedita qui repellat sint, maxime reiciendis accusamus accusantium! Minima autem provident assumenda! Tenetur soluta molestiae voluptate consectetur, blanditiis velit deleniti, odio dignissimos voluptatum repudiandae fugiat natus ipsam delectus aperiam illo! Minima repellat adipisci libero voluptate eveniet inventore consectetur commodi necessitatibus perferendis iusto, quisquam, sapiente eius voluptas esse laboriosam illo consequatur dicta officiis quaerat officia? At quam, molestias deleniti sequi quo ducimus ea minus reprehenderit consectetur.</p>
+        </Layout>
       </div>
     );
   }
