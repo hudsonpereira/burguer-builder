d07286dbd2db991db8c73cc595b7b2ca0d790a5d
Hudson Pereira <hudson.byte@gmail.com>
2017-12-05 10:29:14 -0200

    navigation bar styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from './../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../UI/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../UI/Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  constructor(props) {
+    super(props)
 
-export default layout;
+    this.state = {
+      showSideDrawer: false
+    }
+
+    this.sideDrawerClosedHandler = this.sideDrawerClosedHandler.bind(this)
+    this.drawerToggleClickedHandler = this.drawerToggleClickedHandler.bind(this)
+  }
+
+  sideDrawerClosedHandler() {
+    this.setState({ showSideDrawer: false });
+  }
+
+  drawerToggleClickedHandler() {
+    this.setState((prev, props) => {
+      return {
+        showSideDrawer: ! prev.showSideDrawer
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.drawerToggleClickedHandler} />
+        <SideDrawer show={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+};
+
+export default Layout;
