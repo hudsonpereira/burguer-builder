62060c37f06a26a24bbe321c624c776929aa10f8
Hudson Pereira <hudson.byte@gmail.com>
2017-12-07 15:02:28 -0200

    ❶ first redux middleware

--- src/index.js
+++ src/index.js
@@ -2,18 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action);
 
-store.subscribe(() => {
-  console.log('STORE', store.getState());
-});
+      const result = next(action);
+
+      console.log('[Middleware] State', store.getState());
+      return result;
+    }
+  }
+}
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 const app = (
   <Provider store={store}>
