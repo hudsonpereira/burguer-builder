81694795f6c974e6220af9e0bd6364f58752dc40
Hudson Pereira <hudson.byte@gmail.com>
2017-12-07 19:59:29 -0200

    reset price when setting ingredients

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -36,7 +36,8 @@ const reducer = (state = initialState, action) => {
     case actionTypes.SET_INGREDIENTS:
       return {
         ...state,
-        ingredients: action.ingredients
+        ingredients: action.ingredients,
+        totalPrice: initialState.totalPrice
       }
     case actionTypes.FETCH_INGREDIENTS_FAILED:
       return {
